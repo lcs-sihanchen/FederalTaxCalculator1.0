4f1cdf777cac34e43ec317c9207a3dc6fe62546b
Chen Sihan <schen@lcs.on.ca>
2019-10-31 15:44:54 -0400

    Initial Commit

---
+++ FederalTaxCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FederalTaxCalculator
+//
+//  Created by Chen, Sihan on 2019-10-31.
+//  Copyright © 2019 Chen, Sihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
